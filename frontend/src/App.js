c64d72ef342da78a943285ae277b5c83ca3ce366
philip-zhang <philip@pactinsurance.com>
2021-06-24 20:13:42 +0800

    Added trove size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './bootstrap.css';
 import TotalSupply from './charts/TotalSupply';
 import Holders from './charts/Holders'
+import TroveSize from './charts/TroveSize'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <TotalSupply />
+        <TroveSize />
         <Holders />
       </header>
     </div>
