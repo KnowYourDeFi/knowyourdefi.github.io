75c27d2f06518e301ec54eb7da05c9db201cd40a
philip-zhang <philip@pactinsurance.com>
2021-06-29 12:32:08 +0800

    Added total collateral ratio

--- frontend/src/liquity/charts/CollateralRatio.js
+++ frontend/src/liquity/charts/CollateralRatio.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import ColorProgressBar from '../../widget/ColorProgressBar'
+import {query} from '../LiquityData'
 
 class CollateralRatio extends React.Component {
 
-  // TODO load data
+  state = {
+    loading: true,
+    ratio: 0
+  }
+
+  componentDidMount() {
+      const gql = `
+      {
+        systemStates(first:1, orderBy: sequenceNumber, orderDirection:desc)
+          {
+            totalCollateralRatio
+          }
+      }
+      `
+      //Get Uniswap V2 price
+      query(gql).then(data => {
+          let arr = data.systemStates
+          if (!arr || arr.length === 0) return
+          let ratio = arr[0].totalCollateralRatio
+          this.setState({
+            loading: false,
+            ratio: parseFloat((ratio * 100).toFixed(2))
+          })
+      }).catch(e => {
+          console.error(e)
+      })
+  }
 
   render() {
+    if (this.state.loading) {
+      return <p>Loading...</p>
+    }
+    console.log('this.state.ratio...........', this.state)
     return <ColorProgressBar
-      progress={75}
+      progress={this.state.ratio}
       descriptions={['Killed', 'High Risk', 'Master', 'Healthy']}
     />
   }
