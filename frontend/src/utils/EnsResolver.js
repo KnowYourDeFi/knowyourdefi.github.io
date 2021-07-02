c769ae23ff0c86750d412314f2db5950fd982ead
jzj <jzj1993@gmail.com>
2021-07-03 01:14:00 +0800

    add cache for ens

--- frontend/src/utils/EnsResolver.js
+++ frontend/src/utils/EnsResolver.js
@@ -1,10 +1,39 @@
-import {ensClient} from '../liquity/LiquityData'
+import { ensClient } from '../liquity/LiquityData'
 import { gql } from '@apollo/client'
 
-export async function ensNameToAddress(name) {
-    if (!name) return null
+const NO_ENS = 'no-ens'
 
-    const addressQuery = `
+const ensToAddress = {} // ens -> address
+const addressToEns = {} // address -> ens
+
+export async function ensNameToAddress(ens) {
+  if (!ens) return null
+
+  let address = ensToAddress(ens)
+  if (address) return address
+
+  address = await _ensNameToAddress(ens)
+  ensToAddress[ens] = address
+  addressToEns[address] = ens
+  return address
+}
+
+export async function addressToEnsName(address) {
+  if (!address) return null
+
+  let ens = addressToEns[address]
+  if (ens) return ens === NO_ENS ? null : ens
+
+  ens = _addressToEnsName(address)
+  ensToAddress[ens] = address
+  addressToEns[address] = ens
+  return ens
+}
+
+async function _ensNameToAddress(name) {
+  if (!name) return null
+
+  const addressQuery = `
     query address($name: Bytes!) {
         domains(where:{name:$name})
         {
@@ -19,23 +48,23 @@ export async function ensNameToAddress(name) {
     }
     `
 
-    let result = await ensClient.query({
-        query: gql(addressQuery),
-        variables: {
-            name: name
-        },
-        fetchPolicy: 'cache-first',
-    })
+  let result = await ensClient.query({
+    query: gql(addressQuery),
+    variables: {
+      name: name
+    },
+    fetchPolicy: 'cache-first',
+  })
 
-    const domains = result?.data?.domains
-    if (!domains || domains.length === 0) return null
-    return domains[0].resolver?.addr?.id ?? null
+  const domains = result?.data?.domains
+  if (!domains || domains.length === 0) return null
+  return domains[0].resolver?.addr?.id ?? null
 }
 
-export async function addressToEnsName(address) {
-    if (!address) return null
+async function _addressToEnsName(address) {
+  if (!address) return null
 
-    const nameQuery = `
+  const nameQuery = `
     query address($address: Bytes!) {
         domains(where: {resolvedAddress: $address}) {
           name
@@ -43,17 +72,17 @@ export async function addressToEnsName(address) {
       }
     `
 
-    let result = await ensClient.query({
-        query: gql(nameQuery),
-        variables: {
-            address: address
-        },
-        fetchPolicy: 'cache-first',
-    })
+  let result = await ensClient.query({
+    query: gql(nameQuery),
+    variables: {
+      address: address
+    },
+    fetchPolicy: 'cache-first',
+  })
 
-    const domains = result?.data?.domains
-    if (!domains || domains.length === 0) return null
+  const domains = result?.data?.domains
+  if (!domains || domains.length === 0) return null
 
-    const names = domains.map(domain => domain.name).sort((a, b) => a.length > b.length? 1 : -1)
-    return names[0]
+  const names = domains.map(domain => domain.name).sort((a, b) => a.length > b.length ? 1 : -1)
+  return names[0]
 }
