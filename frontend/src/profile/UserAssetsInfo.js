924ce3abb2fb4dfc922bfc72e82d83aa951ab029
gyczzm <gyczzm@gmail.com>
2021-07-04 03:37:32 +0800

    Updated user profile assets table UI

--- frontend/src/profile/UserAssetsInfo.js
+++ frontend/src/profile/UserAssetsInfo.js
@@ -1,95 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
+import Table from "../liquity/charts/ReactTable"
+import {ellipsesStr, toLink} from '../utils/ChartUtils'
 import axios from 'axios'
 import {numberWithCommas} from '../utils/NumberUtils'
 
-class UserAssetsInfo extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-          loading: true,
-          data: []
-        }
-    }
+function UserAssetsInfo(props) {
+    const [data, setData] = useState([])
 
-    fetchData() {
-      if (!this.props.address) return
-      let url = `https://stg-api.unmarshal.io/v1/ethereum/address/${this.props.address}/assets?auth_key=VGVtcEtleQ%3D%3D`
-      axios.get(url).then(response => {
-        this.setState({
-          loading: false,
-          data: response.data
-        })
-      }).catch(e => {
-        console.error(e)
-      })
-    }
+    useEffect(() => {
+        (async () => {
+          const result = await fetchData(props.address)
+          setData(result);
+        })()
+    }, [props.address])
 
-    componentDidMount() {
-        this.fetchData()
-    }
+    const columns = useMemo(
+        () => [
+            {
+                Header: "Asset",
+                accessor: "info",
+                width: 150,
+                Cell: ({ cell: { value } }) => toLink('https://etherscan.io/token/' + value.contract, ellipsesStr(value.symbol))
+            },
+            {
+                Header: "Balance",
+                accessor: "balance",
+                width: 150
+            },
+            {
+                Header: "Value(USD)",
+                accessor: "value",
+                width: 150
+            }
+        ],
+        []
+    )
+    return (
+    <div className="defi-card">
+        <div className="defi-card-large-title">
+            Ethereum
+        </div>
+        <Table columns={columns} data={data} />
+    </div>
+    )
+}
 
-    componentDidUpdate(prevProps) {
-      if (this.props.address !== prevProps.address) {
-        this.setState({
-          loading: true,
-          data: []
-        })
-        this.fetchData()
+async function fetchData(address) {
+  if (!address) return
+  let url = `https://stg-api.unmarshal.io/v1/ethereum/address/${address}/assets?auth_key=VGVtcEtleQ%3D%3D`
+  try {
+    const response = await axios.get(url)
+    let result = response.data.slice()
+    result = result.map((item) => {
+      const symbol = item.contract_ticker_symbol
+      const decimal = item.contract_decimals
+      const balance = numberWithCommas((item.balance / Math.pow(10, decimal)).toFixed(2))
+      const value = numberWithCommas(item.quote.toFixed(2))
+      return {
+        info: {
+          symbol: symbol,
+          contract: item.contract_address
+        },
+        balance: balance + ' ' + ellipsesStr(symbol),
+        value: '$' + value
       }
-    }
-
-    renderWalletTable() {
-        const loading = this.state.loading
-        if (loading) {
-            return (
-                <tbody>
-                    <tr key='Loading'>
-                        <th>Loading</th>
-                        <td>Loading</td>
-                        <td>Loading</td>
-                      </tr>
-                </tbody>
-            )
-        }
-        return <tbody>
-            {this.state.data.map((item) => {
-              const symbol = item.contract_ticker_symbol
-              const decimal = item.contract_decimals
-              const balance = numberWithCommas((item.balance / Math.pow(10, decimal)).toFixed(2))
-              const value = numberWithCommas(item.quote.toFixed(2))
-              return (
-              <tr key={item.contract_address}>
-                <th>{symbol}</th>
-                <td>
-                    {balance + ' ' + symbol}
-                </td>
-                <td>
-                    {'$' + value}
-                </td>
-              </tr>)
-            })}
-          </tbody>
-    }
-
-    render() {
-        return (
-            <div className="defi-card">
-                <div className="defi-card-large-title">
-                    Ethereum
-                </div>
-                <table className="table">
-                  <thead>
-                    <tr>
-                      <th>Asset</th>
-                      <th>Balance</th>
-                      <th>Value(USD)</th>
-                    </tr>
-                  </thead>
-                  {this.renderWalletTable()}
-                </table>
-            </div>
-        )
-    }
+    })
+    return result
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export default UserAssetsInfo
--- frontend/src/utils/ChartUtils.js
+++ frontend/src/utils/ChartUtils.js
@@ -9,9 +9,9 @@ export const loadingOption = {
     spinnerRadius: 20,
     lineWidth: 5
   }
-export const ellipsesStr = (str) => {return str.substr(0, 6) + '...' + str.substr(str.length-4, str.length)}
+export const ellipsesStr = (str) => {return str.length > 13 ? str.substr(0, 6) + '...' + str.substr(str.length-4, str.length) : str}
 export const toLink = (url, display) => {
     return (
-        <a href={url} target="_blank" rel="noreferrer"> {display} </a>
+        <a href={url} target="_blank" rel="noreferrer" style={{color: chartBlue}}> {display} </a>
     )
 }
